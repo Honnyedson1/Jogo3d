7c333420cfb93dcbc4f5430adda9d8f02a0c1288
Honnyedson <Honnyedson.c@escolar.ifrn.edu.br>
2023-09-15 10:14:58 -0300

    Honny Aula15/09

---
+++ Honnytest/Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController Controler;
+    public float speed;
+    public float smoothRotRime;
+    public float turnsmoothvelocity;
+    private Transform cam;
+    
+    void Start()
+    {
+        Controler = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+    }
+    
+    
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(x, 0f, y);
+
+        if (direction.magnitude > 0)
+        {
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float smothAngle =
+                Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnsmoothvelocity, smoothRotRime);
+            transform.rotation = Quaternion.Euler(0f, smothAngle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+            
+            Controler.Move(moveDirection * speed * Time.deltaTime);
+        }
+    }
+}
